dc87b9b6a525e7536a9c57937256e07f026faa5c
Ankit Kumar <111765525+Ankit-0009@users.noreply.github.com>
2024-03-17 20:18:09 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (56.53%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,25 +1,6 @@
 class Solution {
 public:
-    bool rotateString(string s, string goal) {
-        if(s.length() != goal.length()) return false;
-        if(s == goal) return true;
-        queue<char> q1,q2;
-        for(int i=0;i<s.length();i++){
-            q1.push(s[i]);
-        }
-        for(int i=0;i<goal.length();i++){
-            q2.push(goal[i]);
-        }
-        int k = goal.length() - 1;
-        while(k!=0){
-            char ch = q2.front();
-            q2.pop();
-            q2.push(ch);
-
-            if(q1 == q2) return true;
-
-            k--;
-        }
-        return false;
+    bool rotateString(string A, string B) {
+        return A.size() == B.size() && (A + A).find(B) != string::npos;
     }
 };
