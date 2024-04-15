2a46952904445ed766d22aba910f0801fda47fc5
Ankit Kumar <111765525+Ankit-0009@users.noreply.github.com>
2024-04-15 17:31:38 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (45.82%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -1,19 +1,24 @@
 /*
-    Approach :- Apply algorithm of preorder traversal and store
-    values in the vector.
+    Approach :- Here we apply another approach of Preorder 
+    Traversal, therefore we implement the stack to traversal.
 */
-
 class Solution {
 public:
-    void helper (TreeNode* root, vector<int>& ans) {
-        if(root == NULL) return;
-        ans.push_back(root->val);
-        helper(root->left, ans);
-        helper(root->right, ans);
+    void traversal(TreeNode* root, vector<int>& ans) {
+        stack<TreeNode*> stk;
+        stk.push(root);
+        while(stk.size() > 0) {
+            TreeNode* temp = stk.top();
+            stk.pop();
+            ans.push_back(temp->val);
+            if(temp->right != NULL) stk.push(temp->right);
+            if(temp->left != NULL) stk.push(temp->left);
+        }
     }
     vector<int> preorderTraversal(TreeNode* root) {
+        if(root == NULL) return {};
         vector<int> ans;
-        helper(root, ans);
+        traversal(root, ans);
         return ans;
     }
 };
