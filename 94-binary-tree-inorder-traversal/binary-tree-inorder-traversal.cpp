5aff5967f0edf2e2e01f672cd9ac350039a7ee7b
Ankit Kumar <111765525+Ankit-0009@users.noreply.github.com>
2024-04-15 23:17:05 +0530

    Time: 4 ms (31.98%) | Memory: 9.9 MB (17.67%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -1,19 +1,32 @@
 /*
-    Approach :- Apply algorithm of inorder traversal and store
-    values in the vector.
+    Approach :- Firstly consider a stack that has initially 
+    root value in it. Now iterate a loop in which if current-
+    node is not empty then push current node to stack and 
+    make current node to node ka left node.
+    Else pop out the top node from stack and print or store 
+    that after make current node to node ka right node.
 */
-
 class Solution {
 public:
-    void helper (TreeNode* root, vector<int>& ans) {
-        if(root == NULL) return;
-        helper(root->left, ans);
-        ans.push_back(root->val);
-        helper(root->right, ans);
+    void traversal (TreeNode* root, vector<int>& ans) {
+        stack<TreeNode*> stk;
+        TreeNode* node = root;
+        while(stk.size() > 0 || node != NULL) {
+            if(node != NULL) {
+                stk.push(node);
+                node = node->left;
+            }
+            else {
+                TreeNode* temp = stk.top();
+                stk.pop();
+                ans.push_back(temp->val);
+                node = temp->right;
+            }
+        }
     }
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> ans;
-        helper(root, ans);
+        traversal(root, ans);
         return ans;
     }
 };
