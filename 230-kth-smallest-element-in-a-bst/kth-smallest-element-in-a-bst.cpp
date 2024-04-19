fa40cc182ad8e54f3ffbd3131d93a48a9ce3dc4a
Ankit Kumar <111765525+Ankit-0009@users.noreply.github.com>
2024-04-19 16:33:48 +0530

    Time: 16 ms (34.65%) | Memory: 22.5 MB (60.61%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -1,17 +1,21 @@
+/*
+    Approach :- Inorder traversal in BST leads to sorted array.
+    Toh es property se travel in BST then k ko decrement krte
+    rhnge. When k become 0 so that root->val is our desired value.
+*/
+
 class Solution {
 public:
-    void inorder (TreeNode* root, vector<int>& ans) {
+    void inorder (TreeNode* root, int& k, int& n) {
         if(root == NULL) return;
-        inorder(root->left, ans);
-        ans.push_back(root->val);
-        inorder(root->right, ans);
+        inorder(root->left, k, n);
+        k--;
+        if(k == 0) n = root->val;
+        inorder(root->right, k, n);
     }
     int kthSmallest(TreeNode* root, int k) {
-        vector<int> ans;
-        inorder(root, ans);
-        for(int i=0;i<ans.size();i++) {
-            if(k - 1 == i) return ans[i];
-        }
-        return -1;
+        int n = INT_MAX;
+        inorder(root, k, n);
+        return n;
     }
 };
