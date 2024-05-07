0d2ea04c447779386349bc23cb8d2a9dfff26ed0
Ankit Kumar <111765525+Ankit-0009@users.noreply.github.com>
2024-05-07 10:27:11 +0530

    Time: 14 ms (11.63%) | Memory: 9.7 MB (32.47%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -14,23 +14,18 @@ public:
 
         // Solving this problem using two hashmaps.
         unordered_map<char, int> m1;
-        unordered_map<char, int> m2;
         for(int i=0;i<s.length();i++) {
             m1[s[i]]++;
         }
         for(int i=0;i<t.length();i++) {
-            m2[t[i]]++;
-        }
-
-        for(auto x:m1) {
-            char ch1 = x.first;
-            int freq1 = x.second;
-            if(m2.find(ch1) != m2.end()) {
-                int freq2 = m2[ch1];
-                if(freq1 != freq2) return false;
+            char ch = t[i];
+            if(m1.find(ch) != m1.end()){
+                m1[ch]--;
+                if(m1[ch] == 0) m1.erase(ch);
             }
             else return false;
         }
+        if(m1.size()>0) return false;
         return true;
     }
 };
