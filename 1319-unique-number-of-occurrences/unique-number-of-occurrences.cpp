7d360d0a701037d917d4d543724ca5c1c3e8987f
Ankit Kumar <111765525+Ankit-0009@users.noreply.github.com>
2024-05-09 16:12:40 +0530

    Time: 6 ms (28.77%) | Memory: 11.3 MB (20.64%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -1,16 +1,3 @@
-/*
-    Approach :- Firstly, count the occurence of each element
-    in array. Toh uske liye hashmaps ka use kra (key-value)
-    property ka. Jisme key toh arr[i] and value hogi 'i'.
-    So, now we have to check each element has unique 
-    occurence. Uske liye two unorderd sets banye jisme ek
-    mein toh arr[i] insert krwaye and ek mein ouccrence
-    jo maps mein store kri thi. Toh sets ki property hai ki
-    same element dobara insert ni ho skta. Therefore, 
-    haar element ki unique occurence ke karan dono sets ka
-    size same hona chaiye. Otherwise false hai.
-*/
-
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
@@ -18,13 +5,12 @@ public:
         for(int i=0;i<arr.size();i++) {
             mp[arr[i]]++;
         }
-        unordered_set<int> s1; // For elements
-        for(int i=0;i<arr.size();i++) s1.insert(arr[i]);
-
-        unordered_set<int> s2; // For occurence
-        for(int i=0;i<arr.size();i++) s2.insert(mp[arr[i]]);
-        
-        if(s1.size() == s2.size()) return true;
-        return false;
+        unordered_set<int> s;
+        for(auto x:mp) {
+            int freq = x.second;
+            if(s.find(freq) != s.end()) return false;
+            else s.insert(freq);
+        }
+        return true;
     }
 };
