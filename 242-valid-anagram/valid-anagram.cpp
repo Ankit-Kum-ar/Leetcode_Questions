7f7e4af82bc868f85c74540d0a4c41921636006d
Ankit Kumar <111765525+Ankit-0009@users.noreply.github.com>
2024-05-10 12:15:27 +0530

    Time: 11 ms (37.65%) | Memory: 9.6 MB (32.55%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,31 +1,19 @@
-/*
-    Approach :- The approach is simple by using the two hasmaps.
-    So we count frequency of each character in the map. After 
-    count the frequency we check that each character also present
-    in both string and if they present then check the frequency of
-    that character in both string .If equal then return true
-    otherwise false.
-*/
-
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length() != t.length()) return false;
-
-        // Solving this problem using two hashmaps.
-        unordered_map<char, int> m1;
+        map<char, int> m;
         for(int i=0;i<s.length();i++) {
-            m1[s[i]]++;
+            m[s[i]]++;
         }
         for(int i=0;i<t.length();i++) {
-            char ch = t[i];
-            if(m1.find(ch) != m1.end()){
-                m1[ch]--;
-                if(m1[ch] == 0) m1.erase(ch);
+            int ch = t[i];
+            if(m.find(ch) != m.end()) {
+                m[ch]--;
+                if(m[ch] <= 0) m.erase(ch);
             }
             else return false;
         }
-        if(m1.size()>0) return false;
-        return true;
+        if(m.size() == 0) return true;
+        return false;
     }
 };
