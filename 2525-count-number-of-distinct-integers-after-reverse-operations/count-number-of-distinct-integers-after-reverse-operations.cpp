84ca743c3ad0f4e007bcce6ea7b2df2ba3b7de2e
Ankit Kumar <111765525+Ankit-0009@users.noreply.github.com>
2024-05-10 10:07:09 +0530

    Time: 212 ms (90.54%) | Memory: 110.2 MB (54.87%) - LeetSync

--- 2525-count-number-of-distinct-integers-after-reverse-operations/count-number-of-distinct-integers-after-reverse-operations.cpp
+++ 2525-count-number-of-distinct-integers-after-reverse-operations/count-number-of-distinct-integers-after-reverse-operations.cpp
@@ -1,35 +1,22 @@
-/*
-    Approach :- In this problem, We have to count different elements
-    after inserting reverse of each existing elements. So for that
-    we take help of sets jisme kewal unique elements hi count hote 
-    hai. So push_back all elements in vector after reversing them.
-    Then inserting all elements of vector in unordered set and
-    return the size of set.
-*/
 class Solution {
 public:
-    void rev (vector<int>& nums) {
-        int n = nums.size();
-        for(int i=0;i<n;i++){
-            int x = nums[i];
-            if(x < 10) nums.push_back(x);
-            else {
-                int temp = 0;
-                while(x != 0) {
-                    int rem = x%10;
-                    temp = temp*10 + rem;
-                    x = x/10;
-                }
-                nums.push_back(temp);
-            }
+    int rev (int x) {
+        int temp = 0;
+        while(x != 0) {
+            int rem = x%10;
+            temp = temp * 10 + rem;
+            x = x/10;
         }
+        return temp;
     }
     int countDistinctIntegers(vector<int>& nums) {
-        rev(nums);
-        unordered_set<int> ans;
+        unordered_set<int> s;
         for(int i=0;i<nums.size();i++) {
-            ans.insert(nums[i]);
+            s.insert(nums[i]);
+            if(nums[i] > 9) {
+                s.insert(rev(nums[i]));
+            }
         }
-        return ans.size();
+        return s.size();
     }
 };
