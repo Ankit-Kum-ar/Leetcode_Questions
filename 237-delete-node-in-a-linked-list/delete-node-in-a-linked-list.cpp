c443c2f775f9fde2c03a70964814f284697a2dcc
Ankit Kumar <111765525+Ankit-Kum-ar@users.noreply.github.com>
2024-09-10 14:53:44 +0530

    Time: 10 ms (32.45%) | Memory: 12.8 MB (69.14%) - LeetSync

--- 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
@@ -1,20 +1,15 @@
-/*
-    Approach :- The approach is tricky in this problem. 
-    The problem says that don't remove node from memory now
-    this is essence of problem that we should not remove 
-    linking of node, infact we just simply remove the value
-    from that node. For that we apply a loop where we insert
-    the value of next node to current node and when we reach
-    second last node make it's next => NULL.
-*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-        while(node->next->next != NULL) {
-            node->val = node->next->val;
-            node = node->next;
-        }
-        node->val = node->next->val;
-        node->next = NULL;
+        swap(node->val, node->next->val);
+        node->next = node->next->next;
     }
 };
